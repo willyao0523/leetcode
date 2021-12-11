df45134fe358f4cf462b5ca59ae04ef33160fc82
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-12-11 15:10:15 +0800

    add two pointer algo exercise

---
+++ leetcode-everyday/双指针/153-寻找旋转排序数组中的最小值/153-寻找旋转排序数组中的最小值.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMin = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while(left < right) {
+      let mid = left + Math.floor((right - left)/2);
+      if(nums[mid] < nums[right]) {
+          right = mid;
+      } else {
+          left = mid + 1;
+      }
+  }
+  return nums[left];
+};
---
+++ leetcode-everyday/双指针/154-寻找旋转排序数组中的最小值II/154-寻找旋转排序数组中的最小值II.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMin = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while(left < right) {
+      let mid = left + Math.floor((right - left)/2);
+      if(nums[mid] < nums[right]) {
+          right = mid;
+      } else if(nums[mid] > nums[right]) {
+          left = mid + 1;
+      } else {
+          right --;
+      }
+  }
+  return nums[left];
+};
